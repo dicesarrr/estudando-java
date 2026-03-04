f0735b5fc4f88eb8638571b4cb7062157cc7bd25
Richard Cesar <richardcesar911@gmail.com>
2026-03-04 16:36:56 -0300

    Exemplos de processamento em java

---
+++ fundamentos/processamentos_em_java/src/ProcessamentosEmJava.java
@@ -0,0 +1,28 @@
+
+public class ProcessamentosEmJava {
+	
+	public static void main(String[] args) {
+		
+		// Exemplo de processamento simples
+		int x, y;
+		
+		x = 5;
+		y = x * 2;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		// Exemplo de processamento com Casting (conversão explícita de valores)
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+		
+	}
+	
+}
