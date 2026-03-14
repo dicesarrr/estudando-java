150dd126d7fb741fabd118d006c2d98563bc458c
Richard Cesar <richardcesar911@gmail.com>
2026-03-14 19:24:04 -0300

    Criando uma classe para representar melhor o triângulo na resolução feita em introducao-a-objetos-pt1

--- poo/introducao-a-objetos-pt2/src/application/Main.java
+++ poo/introducao-a-objetos-pt2/src/application/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// Solucionando problema sem orientação a objetos
+		// Solucionando problema com orientação a objetos
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
