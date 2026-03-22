d631f22977cefb75694c4af44b8045f68ed66759
Richard Cesar <richardcesar911@gmail.com>
2026-03-22 09:22:59 -0300

    Entendendo como funciona o comportamento de memória

---
+++ poo/comportamento-de-memoria/src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Isso não vai funcionar, pois a variável precisa ser inicializada
+		int p;
+		System.out.println(p);
+
+	}
+
+}
