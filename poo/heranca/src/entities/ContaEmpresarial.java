c7ee2a1912e01ea7e32687d3f6475ab3b7ce91c4
Richard Cesar <richardcesar911@gmail.com>
2026-05-04 14:48:51 -0300

    Aprendendo o conceito de herança

---
+++ poo/heranca/src/entities/ContaEmpresarial.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class ContaEmpresarial extends Conta{
+
+	private Double limiteDeEmprestimo;
+	
+	public ContaEmpresarial() {
+		super();
+	}
+
+	public ContaEmpresarial(Integer numero, String titular, Double saldo, Double limiteDeEmprestimo) {
+		super(numero, titular, saldo);
+		this.limiteDeEmprestimo = limiteDeEmprestimo;
+	}
+
+	public Double getLimiteDeEmprestimo() {
+		return limiteDeEmprestimo;
+	}
+
+	public void setLimiteDeEmprestimo(Double limiteDeEmprestimo) {
+		this.limiteDeEmprestimo = limiteDeEmprestimo;
+	}
+	
+	public void emprestimo(double quantidade) {
+		if (quantidade <= limiteDeEmprestimo) {
+			saldo += quantidade - 10.0;
+		}
+	}
+	
+}
