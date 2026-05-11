3a5be40dcb4391d0d61cc41c5269d755bf26e200
Richard Cesar <richardcesar911@gmail.com>
2026-05-11 15:01:02 -0300

    Entendendo o a pilha de chamadas de métodos (stack trace)

--- avancado/estrutura-try-catch/src/application/Main.java
+++ avancado/estrutura-try-catch/src/application/Main.java
@@ -6,20 +6,43 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
+		metodo1();
+		
+		System.out.println("Fim do programa!");
+		
+	}
+
+	public static void metodo1() {
+		
+		System.out.println("***METODO 1 INICIADO***");
+		
+		metodo2();
+		
+		System.out.println("***METODO 1 FINALIZADO***");
+	}
+	
+	public static void metodo2() {
+
+		System.out.println("***METODO 2 INICIADO***");
 		
 		Scanner sc = new Scanner(System.in);
-		
+
 		try {
 			String[] vetor = sc.nextLine().split(" ");
 			int posicao = sc.nextInt();
 			System.out.println(vetor[posicao]);
-		} catch(ArrayIndexOutOfBoundsException erro) {
+		} catch (ArrayIndexOutOfBoundsException erro) {
 			System.out.println("Posição inválida, Tente novamente.");
-		} catch(InputMismatchException erro) {
+			erro.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException erro) {
 			System.out.println("Posição inválida, informe um valor numérico.");
 		}
-		
+
 		sc.close();
+		
+		System.out.println("***METODO 2 FINALIZADO***");
 
 	}
 
